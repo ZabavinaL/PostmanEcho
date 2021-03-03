8d9bf9b898b5965552336d08bf7212ca73ec6d80
Lyubov Zabavina <zabavinal@yandex.ru>
2021-03-03 22:00:02 +0300

    PostmanEchoTest edited

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -16,6 +16,6 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"));
+                .body("name", equalTo("some data"));
     }
 }
